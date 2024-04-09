5c5770284a71f2e8cc3bd2a2139f6e103e9aa9dc
Gustavo Rodrigues <rodriguesskt11@gmail.com>
2024-04-09 10:16:43 -0300

    Server

---
+++ projeto_SMP/app.js
@@ -0,0 +1,14 @@
+//fazer a importação do express
+const express = require('express');
+
+//rotas (quando o servidor for acessado, pra onde ele vai?)
+const router = express.Router();
+router.get('/',(req,res)=>{
+    res.send('Olá turma de ADS de SMP de manhã!!');
+});
+
+//configurações básicas do app
+const app = express();
+app.use('/',router);
+
+module.exports = app;
